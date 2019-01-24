3407631b319a5e90d8957e18ff73de75bd848cc9
unknown <m.samael89@gmail.com>
2019-01-23 22:55:11 -0800

    homework

---
+++ app.js
@@ -0,0 +1,12 @@
+//Generate number from 1-10
+
+function Math.floor((Math.random() * 10) + 1);
+console.log(Math.random);
+
+//Reminader of 5/2
+
+function mod(a, b) {
+    return a % b
+}
+
+mod(5, 2)
